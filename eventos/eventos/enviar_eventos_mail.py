236509fdf77fa3813a04ef3bace544ba813e4877
Diego Quispe <diegoar.quispec@gmail.com>
2025-08-14 16:44:20 -0400

    eventos collahuasi

---
+++ eventos/eventos/enviar_eventos_mail.py
@@ -0,0 +1,172 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import yaml
+import pytz
+import argparse
+import subprocess
+from time import sleep
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
+from openai import OpenAI
+from dotenv import load_dotenv
+
+from functions_eventos import DB_API_URL, api_request, mensaje_chat_gpt
+from functions_enviar_eventos import obtener_alertas, generar_destinatario, enviar_correo_html, saludo_inicial_hora
+
+load_dotenv("/usr/smartlink/eventos/.env")
+
+# Leer configuración desde YAML
+script_dir = os.path.dirname(os.path.realpath(__file__))
+with open(f'{script_dir}/configEventos.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# Configuración del parser de argumentos
+parser = argparse.ArgumentParser(description='Enviar mensajes de WhatsApp.')
+parser.add_argument('--num_messages', type=int, help='Número de mensajes máximos.', default=10)
+parser.add_argument('--mudslice_id', type=str, help='ID de MudSlice para enviar mensajes.', default='id_default')
+parser.add_argument('--empresa', type=str, help='Empresa de la alerta.', default='default')
+parser.add_argument('--hours_ago', type=int, help='Número de horas atrás para buscar alertas.', default=1)
+parser.add_argument('--minutes_ago', type=int, help='Número de minutos atrás para buscar alertas.', default=5)
+args = parser.parse_args()
+
+
+# Variables de configuración
+num_messages = args.num_messages
+empresa = args.empresa
+mudslice_id = args.mudslice_id
+
+# Obtener la lista de grupos de WhatsApp desde el YAML
+MUDSLICE_ID_LIST = config['whatsapp']['mudslice'].get(mudslice_id, [])
+
+# Inicializar cliente de OpenAI
+
+api_key=os.getenv("OPEN_API_KEY")
+print("api_key", api_key)
+client = OpenAI(api_key=api_key)
+
+
+def generar_mensaje_correo(html_tablas, empresa):
+    """Genera un mensaje para correo."""
+        
+    mensaje_prompt = f""" 
+    Eres un asistente que generará un informe breve para enviar por email sobre alertas y alarmas relacionados a latencias altas y a interferencias. Estas alertas las detecta un producto de software de HC-GROUP (Asi tal cual, todas las letras en mayusculas y con el guion) llamado "Smartlink" el cual es un software para monitoreo de equipos de telecomunicaciones en faenas mineras. En cuestion haras el resumen del turno, de las ultimas 8 horas.
+    
+    Recuerda ser cordial y presentarte brevemente, y saluda al equipo de {empresa}, en modo de introduccion. En la introduccion menciona que el mensaje proviene de Smartlink de HC-Group para ayudar a {empresa} (solo menciona esto pero en tus palabras no agreges nada mas que sea redundante), y tambien menciona el para que es este mensaje. Se breve. Comienza con algo asi de "Este es un mensaje de ".
+    
+    Los valores estan en estas tablas html:
+    
+    {html_tablas["tabla_señales"]}
+    
+    {html_tablas["tabla_interferencias"]}
+
+    (A veces la tabla puede ser que no se haya generado, en ese caso simplemente pon el mismo mensaje que coloca mi funcion para crear la tabla)
+    
+    Las tablas deben incluirse con su titulo. Si por algun motivo hay valores repetidos en las columnas ip y fecha (fechas exactas con hora incluida), elimina un valor y solo deja el que tenga la recurrencia mas alta.
+    
+    Responde con un mensaje listo para enviar por correo electrónico. ESCRIBELO EN HTML ESTO ES IMPORTANTISIMO.
+    PERO NO AGREGRES ```html NI NADA SIMILAR AL MENSAJE
+    """
+    return mensaje_chat_gpt(client=client, mensaje=mensaje_prompt)
+
+def crear_tabla(alertas, tipo_alerta):
+  """Crea una tabla HTML con columnas adaptadas al tipo de alerta."""
+  print(alertas)
+  tipo_alerta_titulo = f"{tipo_alerta.capitalize()}"
+
+  if not alertas:
+    return f"<h2>{tipo_alerta_titulo}</h2><p>No hay alertas para mostrar.</p>"
+
+  # Columnas base
+  columnas = ["fecha", "estado", "ip", "tag", "marca", "tipo", "recurrencia"]
+
+  # Agregar columnas específicas según tipo
+  if tipo_alerta.lower() == "señal deficiente":
+    columnas += ["latencia"]
+  elif "interferencia" in tipo_alerta.lower():
+    columnas += ["snr_h", "snr_v"]
+  elif "temperatura" in tipo_alerta.lower():
+    columnas += ["temperatura"]
+
+  # Encabezado
+  html = f"<h2 style='color:#2E86C1;'>Alertas de {tipo_alerta_titulo}</h2>"
+  html += "<table border='1' cellpadding='6' cellspacing='0' style='border-collapse: collapse; font-size: 13px; font-family: Arial;'>"
+  html += "<thead style='background-color:#f2f2f2;'><tr>"
+  for col in columnas:
+    nombre_col = "nombre" if col == "tag" else col
+    html += f"<th>{nombre_col.capitalize()}</th>"
+  html += "</tr></thead><tbody>"
+
+  # Filas
+  for alerta in alertas:
+    html += "<tr>"
+    for col in columnas:
+      if col == "latencia":
+        valor_sin_redondear = alerta.get("latencia", "")
+        if valor_sin_redondear != "":
+          valor = str(round(alerta.get("latencia"),2))
+      else:
+        valor = alerta.get(col)
+      if valor is None and isinstance(alerta.get("detalle"), dict):
+        valor = alerta["detalle"].get(col)
+      html += f"<td>{valor if valor is not None else ''}</td>"
+    html += "</tr>"
+
+  html += "</tbody></table><br>"
+  return html
+
+def guardar_como_html(html, nombre_archivo="alertas.html"):
+    with open(nombre_archivo, "w", encoding="utf-8") as f:
+        f.write(f"""
+        <html>
+            <head><meta charset="utf-8"></head>
+            <body>
+                {html}
+            </body>
+        </html>
+        """)
+    print(f"✅ Archivo guardado como {nombre_archivo}")
+
+
+if __name__ == "__main__":
+    # Obtener alertas urgentes
+    remitente = config["email"]["remitente"]
+    clave = config["email"]["smtp_password"]
+    destinatarios = config["email"]["destinatarios"]
+    con_copia = config["email"]["con_copia"]
+    
+    url_alertas = urljoin(DB_API_URL, config["api"]["eventos"]["get"])
+    url_alertas_urgentes = urljoin(DB_API_URL, config["api"]["eventos"]["urgentes"])
+    print(f"url_alertas_urgentes: {url_alertas_urgentes}")
+    señal_deficientes_urgentes, interferencias_urgentes = obtener_alertas(
+        url_alertas=url_alertas_urgentes,
+        horas_atras=args.hours_ago,
+        minutos_atras=args.minutes_ago,
+    )
+    hora_actual = datetime.now().hour
+    saludo_inicial = saludo_inicial_hora(hora_actual)
+
+    crear_tabla_señales = crear_tabla(señal_deficientes_urgentes, "señal deficiente")
+    crear_tabla_interferencias = crear_tabla(interferencias_urgentes, "interferencias")
+
+    guardar_como_html(crear_tabla_señales, "tabla_poor_latency.html")
+    guardar_como_html(crear_tabla_interferencias, "tabla_interferencias.html")
+
+    html_tablas = {
+      "tabla_señales": crear_tabla_señales,
+      "tabla_interferencias": crear_tabla_interferencias
+    }
+
+    mensaje_a_enviar = generar_mensaje_correo(html_tablas, empresa)
+    #mensaje_a_enviar = "Prueba de mensaje"
+    
+
+    enviar_correo_html(
+        remitente=remitente,
+        clave=clave,
+        destinatarios=destinatarios,
+        con_copia=con_copia,
+        asunto=f"Alertas y alarmas de {empresa}",
+        html=mensaje_a_enviar,
+    )
---
+++ eventos/eventos/send_events_mudslice.py
@@ -0,0 +1,171 @@
+﻿#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import yaml
+import pytz
+import argparse
+import subprocess
+from time import sleep
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
+from openai import OpenAI
+from functions_eventos import DB_API_URL, api_request, send_whatsapp_message_mudslide, mensaje_chat_gpt
+
+# Leer configuraciÃ³n desde YAML
+script_dir = os.path.dirname(os.path.realpath(__file__))
+with open(f'{script_dir}/configEventos.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# ConfiguraciÃ³n del parser de argumentos
+parser = argparse.ArgumentParser(description='Enviar mensajes de WhatsApp.')
+parser.add_argument('--num_messages', type=int, help='NÃºmero de mensajes mÃ¡ximos.', default=10)
+parser.add_argument('--mudslice_id', type=str, help='ID de MudSlice para enviar mensajes.', default='id_default')
+parser.add_argument('--empresa', type=str, help='Empresa de la alerta.', default='default')
+parser.add_argument('--hours_ago', type=int, help='NÃºmero de horas atrÃ¡s para buscar alertas.', default=1)
+parser.add_argument('--minutes_ago', type=int, help='NÃºmero de minutos atrÃ¡s para buscar alertas.', default=5)
+args = parser.parse_args()
+
+# Variables de configuraciÃ³n
+num_messages = args.num_messages
+empresa = args.empresa
+mudslice_id = args.mudslice_id
+
+# Obtener la lista de grupos de WhatsApp desde el YAML
+MUDSLICE_ID_LIST = config['whatsapp']['mudslice'].get(mudslice_id, [])
+
+# Inicializar cliente de OpenAI
+
+api_key='CAMBIAR ESTA COSA A GET ENV'
+client = OpenAI(api_key=api_key)
+
+
+def obtener_alertas():
+    """Consulta la API para obtener alertas urgentes."""
+    now = datetime.now() - timedelta(hours=args.hours_ago, minutes=args.minutes_ago)
+    now_str = now.strftime("%Y-%m-%dT%H:%M:%S")  # Convertir a formato ISO 8601
+
+    url_alertas = urljoin(DB_API_URL, config["api"]["eventos"]["urgentes"]) + f"?fecha={now_str}"
+    print(url_alertas)
+    #url_alertas = urljoin(DB_API_URL, config["api"]["eventos"]["urgentes"]) + f"?fecha=2025-03-01T00:00:00"
+    #print(url_alertas)
+    alertas = api_request(url_alertas, dataframe=False)
+
+    if not alertas:
+        print("No hay alertas urgentes.")
+        sys.exit(0)
+
+    # Clasificar alertas
+    senal_deficientes = [i for i in alertas if "señal deficiente" in i.lower()]
+    interferencias = [i for i in alertas if "interferencia" in i.lower()]
+
+    return senal_deficientes, interferencias
+
+def generar_destinatario(numero):
+    if numero == "+56977566595":
+        return "Cristobal"
+    elif numero == "120363027104819888@g.us":
+        return "Collahuasi"
+    elif numero == "+56971083001":
+        return "Ricardo"
+    elif numero == "+56982280571":
+        return "Hector Veliz"
+    elif numero == "+56976426949":
+        return "Daniel"
+    elif numero == "+56939496396":
+        return "equipo del turno de automatizacion de la mina"
+    else:
+        return "Collahuasi"  
+
+
+def generar_mensaje_whatsapp(alertas, tipo_alerta, empresa):
+    """Genera un mensaje para WhatsApp basado en las alertas y el tipo (seÃ±al deficiente o interferencias)."""
+    print(f"Generando mensaje de alerta para {tipo_alerta}. Cantidad de alertas: {len(alertas)}")
+    if not alertas:
+        return None  # No generamos mensaje si no hay alertas
+    if tipo_alerta == "senal deficiente":
+        valor = "latencia"
+    else:
+        valor = "interferecia"
+        
+    mensaje_prompt = f"""
+    Eres un asistente que generarÃ¡ un informe breve para WhatsApp sobre {tipo_alerta}. Estas alertas las detecta un producto de software de HCG Group llamado "Smartlink" el cual es un software para monitoreo de equipos de telecomunicaciones en faenas mineras. En cuestion haras el resumen del turno, de las ultimas 8 horas.
+    
+    
+    Recuerda ser cordial y presentarte brevemente (SIN SALUDAR PORQUE EL SALUDO YA SE HACE ANTES DE QUE TE LLEGUE ESTA INFO A TI), en modo de introduccion. En la introduccion menciona que el mensaje proviene de Smartlink de HCG-Group para ayudar a {empresa} (solo menciona esto pero en tus palabras no agreges nada mas que sea redundante), y tambien menciona el para que es este mensaje. Se breve. Comienza con algo asi de "Este es un mensaje de "
+    
+    AquÃ­ estÃ¡ la lista de eventos:
+    {alertas}
+
+    Genera un mensaje de alerta que resuma los eventos mÃ¡s urgentes, destacando los de mayor prioridad.
+    Usa emojis ðŸ”´ para Alarmas y ðŸŸ¡ para Alertas. Limita el mensaje a {num_messages} eventos.
+    El formato debe ser claro y conciso, como:
+    - [Fecha] ðŸ”´ Equipo X - promedio del valor de {valor} - 5 veces
+    - [Fecha] ðŸŸ¡ Equipo Y - promedio del valor de {valor} - 2 veces
+
+    DETALLE IMPORTANTE, NO REPETIR EL EQUIPO. El equipo en cada mensaje siempre estara entre los strings "Equipo" y "presenta".
+    A veces puede ser que un equipo registre dos fechas. En ese caso solo menciona a ese equipo con su fecha y su valor mas reciente, y su recurrencia mas alta registrada.
+    Por ejemplo si el equipo X tiene una alerta o alarma a las 17:00:00 y luego otra a las 17:15:00, menciona solo la de las 17:15:00. Me explico?
+
+    Recomienda que tomen medidas o que esten atentos a estos equipos.
+    Aclara que es un mensaje automatizado y que no debe responderse.
+    SOLO ESCRIBE EL MENSAJE, NADA MAS.
+    """
+
+    return mensaje_chat_gpt(client=client, mensaje=mensaje_prompt)
+
+def saludo_inicial_hora(hora):
+     if 8 <= hora < 12:
+         return "Buenos dias"
+     elif 12 <= hora <= 20:
+         return "Buenas tardes"
+     else:
+         return "Buenas noches"
+
+
+def enviar_mensajes_whatsapp(mensaje, tipo_alerta, mudslide_id, destinatario, introduccion):
+    """EnvÃ­a el mensaje por WhatsApp solo si existe."""
+    if not mensaje:
+        print(f"âš ï¸ No hay alertas de {tipo_alerta} para enviar.")
+        return  # No ejecuta nada si el mensaje estÃ¡ vacÃ­o
+
+
+    print(f"ðŸ“¤ Enviando mensaje de {tipo_alerta} a {mudslide_id}")
+
+    try:
+        # Intentar enviar con un timeout de 10 segundos
+        mensaje = f"{introduccion}.\n{mensaje}"
+        
+        send_whatsapp_message_mudslide(mudslide_id, mensaje, timeout=60)
+        print(f"âœ… Mensaje de {tipo_alerta} enviado correctamente a {mudslide_id}")
+
+    except TimeoutError:
+        print(f"âš ï¸ Timeout: WhatsApp no respondiÃ³ en 10 segundos para {mudslide_id}.")
+    except Exception as e:
+        print(f"âŒ Error al enviar mensaje a {mudslide_id}: {e}")
+    sleep(2)  # Esperar entre mensajes para evitar bloqueos de WhatsApp
+
+
+# ðŸŸ¢ **Ejecutar el flujo**
+if __name__ == "__main__":
+    senal_deficientes, interferencias = obtener_alertas()
+    hora_actual = datetime.now().hour
+    saludo_inicial = saludo_inicial_hora(hora_actual)
+    mensaje_senales = generar_mensaje_whatsapp(senal_deficientes, "señal deficiente", empresa)
+    mensaje_interferencias = generar_mensaje_whatsapp(interferencias, "interferencias", empresa)
+    for mudslide_id in MUDSLICE_ID_LIST:
+        destinatario = generar_destinatario(mudslide_id)
+        
+        if destinatario == "Collahuasi":
+            introduccion = f"{saludo_inicial} estimado equipo de {destinatario}"
+        elif destinatario == "equipo del turno de automatizacion de la mina":
+            introduccion = f"{saludo_inicial} estimado {destinatario}"
+        else:
+            introduccion = f"{saludo_inicial} estimado {destinatario}"
+
+        print(f"ðŸ“¤ Enviando mensaje de seÃ±al deficiente a {mudslide_id} - {destinatario}")
+        enviar_mensajes_whatsapp(mensaje_senales, "seÃ±al deficiente", mudslide_id, destinatario, introduccion)
+        print(f"ðŸ“¤ Enviando mensaje de interferencias a {mudslide_id} - {destinatario}")
+        enviar_mensajes_whatsapp(mensaje_interferencias, "interferencias", mudslide_id, destinatario, introduccion)
+
+    print("Mensajes enviados correctamente.")
